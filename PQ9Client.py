5526f99f27accbb7f4feb600f5c1736bb3245d33
Stefano Speretta <s.speretta@tudelft.nl>
2022-04-09 22:33:07 +0200

    Transmission is no more async
    This allows a full duplex communication. Added also a connection closed exception.

--- PQ9Client.py
+++ PQ9Client.py
@@ -3,44 +3,44 @@ import json
 
 class PQ9Client:
 
-    def __init__(self, serverIP, serverPort, time):
+    def __init__(self, serverIP, serverPort, timeout):
         super().__init__()
-        self.TIMEOUT = time
+        self.TIMEOUT = timeout
         self.TCP_IP = serverIP
         self.TCP_PORT = serverPort
         self.pq9reader = 0
         self.pq9writer = 0
         self.loop = 0
+        self.closed = True
 
     def close(self):
-        self.loop.run_until_complete(self.AwaitedClose())
+        try:
+            self.closed = True
+            self.loop.run_until_complete(self.AwaitedClose())
+        except RuntimeError:
+            pass # ignore exception
+        except RuntimeWarning:
+            pass # ignore warning
 
     async def AwaitedClose(self):
-        #self.pq9reader.close()
         self.pq9writer.close()
         await self.pq9writer.wait_closed()
     
     def connect(self):
-        #try:
-            self.loop = asyncio.new_event_loop()
-            self.loop.run_until_complete(self.AwaitedConnect())
-        #except ConnectionRefusedError:
-            #print('timeout error')
+        self.loop = asyncio.new_event_loop()
+        self.loop.run_until_complete(self.AwaitedConnect())
 
     async def AwaitedConnect(self):
         self.pq9reader, self.pq9writer = await asyncio.open_connection(self.TCP_IP, self.TCP_PORT, loop=self.loop)
-        print("PQ9Socket: Connected to "+str(self.TCP_IP)+":"+str(self.TCP_PORT))
+        self.closed = False
 
     def sendFrame(self, inputFrame):
-        self.loop.run_until_complete(self.AwaitedSendFrame(inputFrame))
-
-    async def AwaitedSendFrame(self,inputFrame):
         cmdString = json.dumps(inputFrame) + '\n'
-        #print("Sending: "+cmdString, end="")
         self.pq9writer.write(cmdString.encode())
-        await self.pq9writer.drain()
 
     def getFrame(self):
+        if self.closed:
+           raise PQ9ClientConnectionClosed('Connection closed')
         status, msg = self.loop.run_until_complete(self.AwaitedGetFrame())
         if(status == True):
             # return status, json.loads(msg)["_raw_"]
@@ -53,10 +53,20 @@ class PQ9Client:
             rxMsg = await asyncio.wait_for(self.pq9reader.readline(), timeout=self.TIMEOUT)
             return True, rxMsg
         except asyncio.TimeoutError:
-            #print("PQ9EGSE Reply Timeout!")
             return False, []
 
     def processCommand(self, command):
         self.sendFrame(command)
         succes, msg = self.getFrame()
         return succes, msg
+
+class PQ9ClientConnectionClosed(Exception):
+    """Exception raised by PQ9Client.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message=""):
+        self.message = message
+        super().__init__(self.message)
